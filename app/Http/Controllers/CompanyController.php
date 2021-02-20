3d5d8217fbf08bab0beaad73a2d1b5a52e6f7c10
rimantepomeckyte <rimante.pomeckyte@gmail.com>
2021-02-20 21:52:35 +0200

    added middleware

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class CompanyController extends Controller
 {
+    public function __construct()
+    {//
+        $this->middleware('auth');
+    }
+
     public function addCompany()
     {
         return view('pages.add-company');
